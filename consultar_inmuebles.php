f940c2c3ddd3279bfbc91d7e7a8afc7ddfde3b11
Julian Salazar <jsalazar@sena.edu.co>
2024-11-19 09:22:23 -0500

    Consultar inmueble 3

--- consultar_inmuebles.php
+++ consultar_inmuebles.php
@@ -9,8 +9,6 @@ while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
     // echo "ID: " . $row['id'] . " - codigoVivienda: " . $row['codigoVivienda'] . " - Título: " . $row['titulo'] . " - Descripción: " . $row['descripcion'] . "<br>";
 }
 
-
-
 // Return the results as a JSON object
 header('Content-Type: application/json');
 echo json_encode($results);
