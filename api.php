e70ec9c2e715b59124e371a92ed7beec1c928aa7
Julian Salazar <jsalazar@sena.edu.co>
2024-11-19 08:51:16 -0500

    Initial commit

---
+++ api.php
@@ -0,0 +1,11 @@
+<?php 
+
+$user = "mysql"; // 
+$password = "ee19d655f1934da88398";
+$server = "localhost";
+$bd = "sena";
+$con = mysqli_connect($server, $user, $password,$bd);
+
+if($con){
+    
+}
