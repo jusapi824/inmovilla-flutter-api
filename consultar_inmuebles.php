7978b07f28be81b515f6ca71f9e6f4a9fabb6a44
Julian Salazar <jsalazar@sena.edu.co>
2024-11-21 09:33:13 -0500

    Headers

--- consultar_inmuebles.php
+++ consultar_inmuebles.php
@@ -1,4 +1,10 @@
 <?php
+header("Access-Control-Allow-Origin: *"); // Permitir acceso desde cualquier origen
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // Métodos permitidos
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With"); // Encabezados permitidos
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+
 include_once "connection.php";
 // Simple query to fetch data from a table
 $query = $pdo->query("SELECT * FROM inmueble");
@@ -10,7 +16,7 @@ while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
 }
 
 // Return the results as a JSON object
-header('Content-Type: application/json');
+
 echo json_encode($results);
 
 // Close the connection
